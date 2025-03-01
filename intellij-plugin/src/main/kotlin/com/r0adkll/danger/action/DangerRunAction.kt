053a08debda15823390f8c96a578f6dd7b0e8ac9
r0adkll <veedubusc@gmail.com>
2025-03-01 13:20:54 -0500

    Make danger executable from gutter icon, using danger local for now

--- intellij-plugin/src/main/kotlin/com/r0adkll/danger/action/DangerRunAction.kt
+++ intellij-plugin/src/main/kotlin/com/r0adkll/danger/action/DangerRunAction.kt
@@ -1,27 +1,36 @@
 package com.r0adkll.danger.action
 
+import com.intellij.execution.ProgramRunnerUtil
+import com.intellij.execution.RunManager
+import com.intellij.execution.executors.DefaultRunExecutor
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.psi.PsiElement
-import com.r0adkll.danger.services.dangerKotlin
+import com.r0adkll.danger.run.DangerRunConfiguration
+import com.r0adkll.danger.run.DangerRunConfigurationType
+import kotlin.io.path.absolutePathString
+import kotlin.io.path.name
 
 class DangerRunAction(private val dangerEntrypointElement: PsiElement) : AnAction() {
 
   override fun actionPerformed(event: AnActionEvent) {
-    val dangerFile = dangerEntrypointElement.containingFile.originalFile.virtualFile.path
-    thisLogger().warn("Running Dangerfile: ${dangerFile}")
+    val dangerFile = dangerEntrypointElement.containingFile.originalFile.virtualFile.toNioPath()
+    thisLogger().info("Running Dangerfile: ${dangerFile}")
 
-    val danger = event.project?.dangerKotlin() ?: return
+    // Create and add the run configuration
+    val runManager = RunManager.getInstance(event.project!!)
+    val configuration = runManager.createConfiguration(
+      "Run ${dangerFile.fileName.name}",
+      DangerRunConfigurationType::class.java,
+    )
+    runManager.addConfiguration(configuration)
 
-    if (danger.exists()) {
-      danger.runLocal(dangerFile)
-    } else {
-      // TODO: We should show a more graceful handling of this, whether an error notification
-      //  or prompt UI to instruct user to install danger/danger-kotlin
-      error("danger-kotlin is not installed!")
-    }
+    // Hydrate with the passed dangerfile
+    val dangerRunConfiguration = (configuration.configuration as DangerRunConfiguration)
+    dangerRunConfiguration.dangerFilePath = dangerFile.absolutePathString()
 
-    TODO("Not yet implemented")
+    // Execute
+    ProgramRunnerUtil.executeConfiguration(configuration, DefaultRunExecutor.getRunExecutorInstance())
   }
 }
---
+++ intellij-plugin/src/main/kotlin/com/r0adkll/danger/run/DangerRunConfiguration.kt
@@ -0,0 +1,131 @@
+package com.r0adkll.danger.run
+
+import com.intellij.execution.Executor
+import com.intellij.execution.configurations.*
+import com.intellij.execution.process.ProcessHandler
+import com.intellij.execution.process.ProcessHandlerFactory
+import com.intellij.execution.process.ProcessTerminatedListener
+import com.intellij.execution.runners.ExecutionEnvironment
+import com.intellij.icons.AllIcons
+import com.intellij.openapi.components.BaseState
+import com.intellij.openapi.fileChooser.FileChooserDescriptorFactory
+import com.intellij.openapi.options.SettingsEditor
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.ui.TextFieldWithBrowseButton
+import com.intellij.openapi.util.NotNullLazyValue
+import com.intellij.openapi.util.io.toNioPathOrNull
+import com.intellij.util.ui.FormBuilder
+import javax.swing.JComponent
+import javax.swing.JPanel
+
+class DangerRunConfigurationType : ConfigurationTypeBase(
+  id = ID,
+  displayName = "Danger Run",
+  description = "Run a Dangerfile.df.kts",
+  icon = NotNullLazyValue.createValue { AllIcons.Nodes.Console },
+) {
+  init {
+    addFactory(DangerKotlinRunConfigurationFactory(this))
+  }
+
+  companion object {
+    const val ID = "DangerRunConfiguration"
+  }
+}
+
+class DangerKotlinRunConfigurationFactory(type: ConfigurationType) : ConfigurationFactory(type) {
+
+  override fun getId(): String = DangerRunConfigurationType.ID
+
+  override fun createTemplateConfiguration(project: Project): RunConfiguration {
+    return DangerRunConfiguration(project, this, "Run Danger")
+  }
+
+  override fun getOptionsClass(): Class<out BaseState> = DangerRunConfigurationOptions::class.java
+}
+
+class DangerRunConfigurationOptions : RunConfigurationOptions() {
+
+  private val _dangerFilePath = string(null)
+    .provideDelegate(this, "dangerFileName")
+
+  var dangerFilePath: String?
+    get() = _dangerFilePath.getValue(this)
+    set(value) {
+      _dangerFilePath.setValue(this, value)
+    }
+}
+
+class DangerRunConfiguration(
+  project: Project,
+  factory: ConfigurationFactory?,
+  name: String?,
+) : RunConfigurationBase<DangerRunConfigurationOptions>(project, factory, name) {
+
+  var dangerFilePath: String?
+    get() = options.dangerFilePath
+    set(value) {
+      options.dangerFilePath = value
+    }
+
+  override fun getOptions(): DangerRunConfigurationOptions {
+    return super.getOptions() as DangerRunConfigurationOptions
+  }
+
+  override fun getState(executor: Executor, env: ExecutionEnvironment): RunProfileState? {
+    return object : CommandLineState(env) {
+
+      override fun startProcess(): ProcessHandler {
+        val commandLine = GeneralCommandLine(
+          "danger-kotlin",
+          "local",
+          "--base",
+          "main",
+          "-d",
+          options.dangerFilePath,
+        ).apply {
+          withWorkingDirectory(project.basePath?.toNioPathOrNull())
+          withParentEnvironmentType(GeneralCommandLine.ParentEnvironmentType.SYSTEM)
+        }
+
+        val processHandler = ProcessHandlerFactory.getInstance()
+          .createColoredProcessHandler(commandLine)
+        ProcessTerminatedListener.attach(processHandler)
+        return processHandler
+      }
+    }
+  }
+
+  override fun getConfigurationEditor(): SettingsEditor<out RunConfiguration> {
+    return DangerRunSettingsEditor()
+  }
+}
+
+class DangerRunSettingsEditor : SettingsEditor<DangerRunConfiguration>() {
+
+  private val panel: JPanel
+  private val dangerFilePathField: TextFieldWithBrowseButton = TextFieldWithBrowseButton()
+
+  init {
+    dangerFilePathField.addBrowseFolderListener(
+      null,
+      FileChooserDescriptorFactory.createSingleFileDescriptor(".df.kts"),
+    )
+    panel = FormBuilder.createFormBuilder()
+      .addLabeledComponent("Dangerfile", dangerFilePathField)
+      .panel
+  }
+
+  override fun resetEditorFrom(config: DangerRunConfiguration) {
+    dangerFilePathField.setText(config.dangerFilePath)
+  }
+
+  override fun applyEditorTo(config: DangerRunConfiguration) {
+    config.dangerFilePath = dangerFilePathField.text
+  }
+
+  override fun createEditor(): JComponent {
+    return panel
+  }
+
+}
